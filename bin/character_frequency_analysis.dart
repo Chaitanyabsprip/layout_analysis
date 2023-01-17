872e14ad4c34712270b535f5d7d79d09182fcb9a
Chaitanya <chaitanya@zellerfeld.com>
2023-01-18 01:30:40 +0530

    initial commit

---
+++ bin/character_frequency_analysis.dart
@@ -0,0 +1,5 @@
+import 'package:character_frequency_analysis/character_frequency_analysis.dart' as character_frequency_analysis;
+
+void main(List<String> arguments) {
+  print('Hello world: ${character_frequency_analysis.calculate()}!');
+}
---
+++ test/character_frequency_analysis_test.dart
@@ -0,0 +1,8 @@
+import 'package:character_frequency_analysis/character_frequency_analysis.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
