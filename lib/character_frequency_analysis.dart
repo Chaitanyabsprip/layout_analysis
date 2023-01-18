f0f8046969d0592d407cf0e38e4e95154bd1f89a
Chaitanya <chaitanya@zellerfeld.com>
2023-01-18 22:31:35 +0530

    feat: get unigram frequency

--- lib/character_frequency_analysis.dart
+++ lib/character_frequency_analysis.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+class Analyser {
+  Analyser(this.content);
+  final String content;
+  Map<String, int> get characterFrequency {
+    if (content.isEmpty) return {};
+    return content
+        .split('')
+        .asMap()
+        .map((k, v) => MapEntry(v, content.split(v).length - 1))
+      ..remove(' ');
+  }
 }
--- test/character_frequency_analysis_test.dart
+++ test/character_frequency_analysis_test.dart
@@ -1,8 +1,41 @@
 import 'package:character_frequency_analysis/character_frequency_analysis.dart';
 import 'package:test/test.dart';
 
+// should be able to get unigram frequency in absolute number
+// should be able to get unigram frequency in normalised form
+// should be able to get unigram frequency in percentage form
+// should be able to get bigram frequency in absolute number
+// should be able to get bigram frequency in normalised form
+// should be able to get bigram frequency in percentage form
+// should be able to get finger wise sfb percentage
+// should be able to get total sfb percentage
+// should be able to get top ten sfb percentage
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('should be able to get unigram frequency in absolute number', () {
+    test('when input is empty, output should be {}', () {
+      final analyser = Analyser('');
+      expect(analyser.characterFrequency, equals({}));
+    });
+
+    test('when input is "a", output should be {"a": 1}', () {
+      final analyser = Analyser('a');
+      expect(analyser.characterFrequency, equals({'a': 1}));
+    });
+
+    test('when input is "aa", output should be {"a": 2}', () {
+      final analyser = Analyser('aa');
+      expect(analyser.characterFrequency, equals({'a': 2}));
+    });
+
+    test('when input is "ba", output should be {"a": 1, "b": 1}', () {
+      final analyser = Analyser('ba');
+      expect(analyser.characterFrequency, equals({'a': 1, 'b': 1}));
+    });
+
+    test('when input is "a b", output should be {"a": 1, "b": 1}', () {
+      final analyser = Analyser('a b');
+      expect(analyser.characterFrequency, equals({'a': 1, 'b': 1}));
+    });
   });
 }
