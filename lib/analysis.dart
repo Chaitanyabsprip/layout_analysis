c3ae5c9dbddfa126d0a3d0db224d07f45e26ccac
Chaitanya <chaitanya@zellerfeld.com>
2023-01-23 23:41:35 +0530

    feat: added inroll rating as a statistic

--- lib/analysis.dart
+++ lib/analysis.dart
@@ -1,5 +1,5 @@
 import 'frequency.dart';
-import 'keymap.dart';
+import 'layout.dart';
 
 class Analysis {
   Analysis(this.layout, this.frequency);
@@ -7,9 +7,20 @@ class Analysis {
   final Layout layout;
   final Frequency frequency;
 
+  double get inrollRating {
+    if (layout.keymap.first.length < 2) return 0;
+    final ngramNormalised = _ngramNormalised2;
+    return layout.inrolls.fold(
+      0,
+      (rating, bigram) => (ngramNormalised[bigram] ?? 0) + rating,
+    );
+  }
+
+  NormalisedMap get _ngramNormalised2 => frequency.ngramNormalised(2);
+
   double get sfbRating {
     if (layout.keymap.length < 2) return 0;
-    final ngramNormalised = frequency.ngramNormalised(2);
+    final ngramNormalised = _ngramNormalised2;
     return layout.sfb.fold(
       0,
       (rating, sfb) => (ngramNormalised[sfb] ?? 0) + rating,
--- test/analysis_test.dart
+++ test/analysis_test.dart
@@ -1,11 +1,12 @@
 import 'package:character_frequency_analysis/analysis.dart';
 import 'package:character_frequency_analysis/frequency.dart';
-import 'package:character_frequency_analysis/keymap.dart';
+import 'package:character_frequency_analysis/layout.dart';
 import 'package:test/test.dart';
 
 void main() {
   // for analysis, I need the frequency of ngrams, for that I need a corpus
   // for analysis, I also need the layout.
+
   group('get single finger bigram(SFB)', () {
     group('when corpus is "abc", ', () {
       final frequency = Frequency('abc');
@@ -41,6 +42,64 @@ void main() {
           frequency,
         );
         expect(analysis.sfbRating, 1);
+      });
+    });
+  });
+
+  group('get inroll statistic', () {
+    group('when corpus is "abcdefghi"', () {
+      final frequency = Frequency('abcdefghi');
+
+      test('when input layout is [["a"], ["e"]], output should be 0', () {
+        final analysis = Analysis(
+          Layout([
+            ['a'],
+            ['e']
+          ]),
+          frequency,
+        );
+        expect(analysis.inrollRating, 0);
+      });
+
+      test(
+          'when input layout is [["a", "b"], ["e", "f"]],'
+          ' output should be 0', () {
+        final analysis = Analysis(
+          Layout([
+            ['a', 'b'],
+            ['e', 'f']
+          ]),
+          frequency,
+        );
+        expect(analysis.inrollRating, 0);
+      });
+
+      test(
+          'when input layout is [["a", "b" "c", "d"], ["e", "f", "g", "h"]],'
+          ' output should be 0.25', () {
+        print(frequency.ngramNormalised(2));
+        final analysis = Analysis(
+          Layout([
+            ['a', 'b', 'c', 'd'],
+            ['e', 'f', 'g', 'h']
+          ]),
+          frequency,
+        );
+        expect(analysis.inrollRating, 0.25);
+      });
+
+      test(
+          'when input layout is [["a", "b" "d", "c"], ["e", "f", "h", "g"]],'
+          ' output should be 0.5', () {
+        print(frequency.ngramNormalised(2));
+        final analysis = Analysis(
+          Layout([
+            ['a', 'b', 'd', 'c'],
+            ['e', 'f', 'h', 'g']
+          ]),
+          frequency,
+        );
+        expect(analysis.inrollRating, 0.5);
       });
     });
   });
