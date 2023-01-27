2f993407f5585b3149dd715279c7203a61d37390
Chaitanya <chaitanya@zellerfeld.com>
2023-01-27 18:24:48 +0530

    feat: ignore hidden filed for corpus

--- lib/corpus.dart
+++ lib/corpus.dart
@@ -21,6 +21,10 @@ class Corpus {
   void _prepare() {
     for (final path in List<String>.from(paths)) {
       final file = File(path);
+      if (path.isEmpty || (file.isHidden && path != '.')) {
+        paths.remove(path);
+        continue;
+      }
       final isDirectory =
           file.statSync().type == FileSystemEntityType.directory;
       if (isDirectory) _flattenDirectoryFiles(path);
@@ -30,7 +34,11 @@ class Corpus {
   void _flattenDirectoryFiles(String path) {
     paths.remove(path);
     for (final file in Directory(path).listSync(recursive: true)) {
-      if (file is File) paths.add(file.path);
+      if (file is File && !file.isHidden) paths.add(file.path);
     }
   }
+}
+
+extension IsHidden on FileSystemEntity {
+  bool get isHidden => uri.pathSegments.any((e) => e.startsWith('.'));
 }
--- test/corpus_test.dart
+++ test/corpus_test.dart
@@ -3,6 +3,16 @@ import 'package:test/test.dart';
 
 void main() {
   group('load content of file', () {
+    test('when input is empty, should throw assertionError', () {
+      expect(Corpus(['']).load(), '');
+    });
+
+    test(
+        'when input is "test.file1, empty string", should throw assertionError',
+        () {
+      expect(Corpus(['test.file1', '']).load(), '');
+    });
+
     test(r'when input is "test.file1", output should be test1\n', () {
       final corpus = Corpus(['test/fixtures/test.file1']);
       expect(corpus.load(), 'test1\n');
@@ -42,6 +52,37 @@ void main() {
 
     test("when input directory doesn't exist, output should be ''", () {
       final corpus = Corpus(['test/fixtures/test.invalidDirectory']);
+      expect(corpus.load(), '');
+    });
+  });
+
+  group('ignore hidden files', () {
+    test('when input is .test.hiddenFile, output should be ""', () {
+      final corpus = Corpus(['test/fixtures/.test.hiddenFile']);
+      expect(corpus.load(), '');
+    });
+
+    test('when input is .test.hiddenDirectory, output should be ""', () {
+      final corpus = Corpus(['test/fixtures/.test.hiddenDirectory']);
+      expect(corpus.load(), '');
+    });
+
+    test('when input is .test.hiddenDirectory/test.file1, output should be ""',
+        () {
+      final corpus = Corpus(['test/fixtures/.test.hiddenDirectory/test.file1']);
+      expect(corpus.load(), '');
+    });
+
+    test('when input is ".", output should consider current directory files',
+        () {
+      final corpus = Corpus(['.']);
+      expect(corpus.load(), isNotEmpty);
+    });
+
+    test(
+        'when input directory has a hidden directory inside, output should not '
+        'consider the hidden directory', () {
+      final corpus = Corpus(['test/fixtures/test.containsHiddenDirectory']);
       expect(corpus.load(), '');
     });
   });
